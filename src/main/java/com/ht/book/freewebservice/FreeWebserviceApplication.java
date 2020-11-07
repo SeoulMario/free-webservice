5f5b780dc915b2f978e8accadce74d3175c00ec8
ht <heetaeyang0827@gamil.com>
2020-11-07 10:18:35 +0900

    init commit

---
+++ src/main/java/com/ht/book/freewebservice/FreeWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ht.book.freewebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class    FreeWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeWebserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ht/book/freewebservice/FreeWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ht.book.freewebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
